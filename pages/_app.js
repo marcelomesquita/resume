a7efe7ce326f5f4b1158fbffa5884366eac451ac
Marcelo Mesquita <contato@marcelomesquita.com>
2021-09-16 00:05:39 -0300

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react'
+import { analytics } from '/adapters/firebaseClient';
+
+import '/assets/styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const routers = useRouter();
+
+  useEffect(() => {
+    typeof document !== undefined ? require('bootstrap/dist/js/bootstrap') : null;
+
+    if (process.env.NEXT_PUBLIC_PRODUCTION) {
+      const logEvent = (url) => {
+        analytics().setCurrentScreen(url);
+        analytics().logEvent('screen_view');
+      };
+
+      routers.events.on('routeChangeComplete', logEvent);
+
+      logEvent(window.location.pathname);
+
+      return () => {
+        routers.events.off('routeChangeComplete', logEvent);
+      };
+		}
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="author" content="Marcelo Mesquita" />
+        <meta name="description" content="" />
+
+        <title>Marcelo Mesquita</title>
+        <link rel="icon" type="image/x-icon" href="favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
